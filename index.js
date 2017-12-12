5f275f77cd7d0203f443b5ee06d179af028ccb88
Liz Verde <>
2017-12-12 21:17:04 +0000

    Importing Provider, createStore and the reducer into the index.js file, then wrapping the whole app with the Provider, which allows any components inside the app to use data from the store

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import PlayerReducer from './src/reducers/player';
 import Scoreboard from './src/containers/Scoreboard';
 
+const store = createStore(
+	PlayerReducer
+);
+
+//by wrapping the Scoreboard with the Provider it allows any component inside to use the store state globally.
 
 render(
-	<Scoreboard />,
-	document.getElementById('root')
+	<Provider store={ store }> 
+		<Scoreboard />,
+		document.getElementById('root')
+	</Provider>	
 );
